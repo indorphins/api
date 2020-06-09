d9ea0be2d13f4ef891718c96cd34bc968a464c13
afloesch <andrew@vapi.com>
2020-06-08 19:34:53 -0700

    modify handler

--- src/handlers/opentok.js
+++ src/handlers/opentok.js
@@ -99,8 +99,10 @@ async function joinSession(req, res) {
 
   let now = new Date();
   let start = new Date(c.start_date);
-  let startWindow = start.setMinutes(start.getMinutes() - sessionWindow);
-  let endWindow = start.setMinutes(start.getMinutes() + c.duration + sessionWindow);
+  let end = new Date(c.start_date);
+  end.setMinutes(end.getMinutes() + c.duration);
+  let startWindow = new Date(start.setMinutes(start.getMinutes() - sessionWindow));
+  let endWindow = new Date(end.setMinutes(end.getMinutes() + sessionWindow));
 
   // if it's a recurring class and the first class is in the past
   if (c.recurring && now > endWindow) {
@@ -108,14 +110,18 @@ async function joinSession(req, res) {
     // get the previous event date for the recurring class in case there is an
     // active session right now
     start = getPrevDate(c.recurring, 1, now);
-    startWindow = start.setMinutes(start.getMinutes() - sessionWindow);
-    endWindow = start.setMinutes(start.getMinutes() + c.duration + sessionWindow);
+    end = new Date(start);
+    end.setMinutes(end.getMinutes() + c.duration);
+    startWindow = new Date(start.setMinutes(start.getMinutes() - sessionWindow));
+    endWindow = new Date(end.setMinutes(end.getMinutes() + sessionWindow));
 
     // if the prev session is over then get the next session
     if (now > endWindow) {
       start = getNextDate(c.recurring, 1, now);
-      startWindow = start.setMinutes(start.getMinutes() - sessionWindow);
-      endWindow = start.setMinutes(start.getMinutes() + c.duration + sessionWindow);
+      end = new Date(start);
+      end.setMinutes(end.getMinutes() + c.duration);
+      startWindow = new Date(start.setMinutes(start.getMinutes() - sessionWindow));
+      endWindow = new Date(end.setMinutes(end.getMinutes() + sessionWindow));
     }
   }
 
