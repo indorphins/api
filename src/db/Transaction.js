ce0845d7b38c0cd5b8a8caff4903c4206724726b
Alex Lindsay <alindsaydev@gmail.com>
2020-06-29 22:26:34 -0700

    updateClass update

--- src/db/Transaction.js
+++ src/db/Transaction.js
@@ -1,33 +1,36 @@
 const mongoose = require('mongoose');
 
 const Transaction = new mongoose.Schema({
-	classId: {
-		type: String,
-		required: true,
-	},
-	userId: {
-		type: String,
-		required: true,
-	},
-	stripeId: {
-		type: String,
-		required: true,
-	},
-	paymentId: {
-		type: String,
-		unique: true,
-	},
-	subscriptionId: {
-		type: String,
-		unique: true,
-	},
-	status: {
-		type: String,
-	},
-	// Type values: invoice, payment, subscription
-	type: {
-		type: String,
-	},
+  classId: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  stripeId: {
+    type: String,
+    required: true,
+  },
+  paymentId: {
+    type: String,
+    unique: true,
+    sparse: true,
+    index: true
+  },
+  subscriptionId: {
+    type: String,
+    unique: true,
+    sparse: true
+  },
+  status: {
+    type: String,
+  },
+  // Type values: invoice, payment, subscription
+  type: {
+    type: String,
+  },
 });
 
 Transaction.index({ classId: 1, userId: 1 }); // Needed still?
--- src/handlers/class.js
+++ src/handlers/class.js
@@ -8,18 +8,18 @@ const log = require('../log');
  * @param {String} value - base64 encoded and stringified json object representing a valid mongo filter or sort object
  */
 function decodeQueryParam(value) {
-	let buff = new Buffer.from(value, 'base64');
-	let data = null;
+  let buff = new Buffer.from(value, 'base64');
+  let data = null;
 
-	try {
-		data = buff.toString('utf-8');
-		data = JSON.parse(data);
-	} catch(err) {
-		log.warn("invalid encoded object", err);
-		return null;
-	}
+  try {
+    data = buff.toString('utf-8');
+    data = JSON.parse(data);
+  } catch (err) {
+    log.warn("invalid encoded object", err);
+    return null;
+  }
 
-	return data;
+  return data;
 }
 
 /**
@@ -30,46 +30,46 @@ function decodeQueryParam(value) {
  * @param {Object} res - http response object
  */
 async function getClasses(req, res) {
-	
-	let page = req.query.page ? Number(req.query.page) - 1 : 0;
-	let limit = req.query.limit ? Number(req.query.limit) : 50;
-	let order = /*{ start_date: "desc", name: "asc" };*/ {};
-	let filter = /*{ start_date: { $gte : new Date().toISOString() }, available_spots: { $gt: 0 }}*/ {};
 
-	if (req.query.filter) {
-		let data = decodeQueryParam(req.query.filter);
+  let page = req.query.page ? Number(req.query.page) - 1 : 0;
+  let limit = req.query.limit ? Number(req.query.limit) : 50;
+  let order = /*{ start_date: "desc", name: "asc" };*/ {};
+  let filter = /*{ start_date: { $gte : new Date().toISOString() }, available_spots: { $gt: 0 }}*/ {};
 
-		if (data) {
-			filter = data;
-		}
-	}
+  if (req.query.filter) {
+    let data = decodeQueryParam(req.query.filter);
 
-	if (req.query.order) {
-		let data = decodeQueryParam(req.query.order);
+    if (data) {
+      filter = data;
+    }
+  }
 
-		if (data) {
-			order = data;
-		}
-	}
+  if (req.query.order) {
+    let data = decodeQueryParam(req.query.order);
 
-	let total;
-	let results;
+    if (data) {
+      order = data;
+    }
+  }
 
-	try {
-		total = await Class.find(filter).countDocuments();
-		results = await Class.find(filter).sort(order).skip(page*limit).limit(limit);
-	} catch (err) {
-		res.status(500).json({
-			message: err,
-		});
-	}
+  let total;
+  let results;
 
-	res.status(200).json({
-		total: total,
-		page: page + 1,
-		limit: limit,
-		data: results,
-	});
+  try {
+    total = await Class.find(filter).countDocuments();
+    results = await Class.find(filter).sort(order).skip(page * limit).limit(limit);
+  } catch (err) {
+    res.status(500).json({
+      message: err,
+    });
+  }
+
+  res.status(200).json({
+    total: total,
+    page: page + 1,
+    limit: limit,
+    data: results,
+  });
 };
 
 /**
@@ -80,30 +80,30 @@ async function getClasses(req, res) {
  */
 async function createClass(req, res) {
 
-	let classData = req.body;
-	let newClass = null;
+  let classData = req.body;
+  let newClass = null;
 
-	classData.id = uuid.v1();
-	classData.created_date = new Date().toISOString();
-	classData.available_spots = classData.total_spots;
-	classData.instructor = req.ctx.userData;
-	classData.participants = [];
+  classData.id = uuid.v1();
+  classData.created_date = new Date().toISOString();
+  classData.available_spots = classData.total_spots;
+  classData.instructor = req.ctx.userData;
+  classData.participants = [];
 
-	try {
-		newClass = await Class.create(classData);
-	} catch (err) {
-		log.warn('Error creating class: ', err);
-		return res.status(400).json({
-			message: "issue creating class",
-			error: err
-		});
-	}
+  try {
+    newClass = await Class.create(classData);
+  } catch (err) {
+    log.warn('Error creating class: ', err);
+    return res.status(400).json({
+      message: "issue creating class",
+      error: err
+    });
+  }
 
-	log.debug('New class created', newClass);
-	res.status(201).json({
-		message: "New class added",
-		data: newClass,
-	});
+  log.debug('New class created', newClass);
+  res.status(201).json({
+    message: "New class added",
+    data: newClass,
+  });
 };
 
 /**
@@ -113,31 +113,33 @@ async function createClass(req, res) {
  */
 async function getClass(req, res) {
 
-	let filter = { id: req.params.id };
-	let c = null;
-	let i = null;
+  let filter = { id: req.params.id };
+  let c = null;
+  let i = null;
 
-	try {
-		c = await Class.findOne(filter);
-	} catch (err) {
-		log.warn("error fetching class by id", err);
-		return res.status(404).json({
-			message: "Class not found",
-		});
-	}
+  try {
+    c = await Class.findOne(filter);
+  } catch (err) {
+    log.warn("error fetching class by id", err);
+    return res.status(404).json({
+      message: "Class not found",
+    });
+  }
 
-	try {
-		i = await User.findOne({ _id: c.instructor})
-	} catch (err) {
-		log.warn("error fetching instructor", err);
-		return res.status(404).json({
-			message: "Class not found",
-		});
-	}
+  try {
+    i = await User.findOne({ _id: c.instructor })
+  } catch (err) {
+    log.warn("error fetching instructor", err);
+    return res.status(404).json({
+      message: "Class not found",
+    });
+  }
 
-	c.instructor = i;
+  c.instructor = i;
 
-	res.status(200).json(c);
+  console.log("Got Class: ", c);
+
+  res.status(200).json(c);
 };
 
 /**
@@ -147,41 +149,38 @@ async function getClass(req, res) {
  */
 async function updateClass(req, res) {
 
-	let c = null;
-	let data = req.body;
+  let c = null;
+  let data = req.body;
+  let id = req.params.id ? req.params.id : req.ctx.classId;
 
-	if (data.participants) {
-		delete data.participants;
-	}
+  console.log("Instructor is ", req.ctx.userData);
+  console.log('class id is ', req.params.id);
+  console.log("Update data is ", data)
+  console.log("Context class is ", req.ctx.classId);
 
-	// NOTE: this is a hack
-	if (!data.session) {
-		let session;
-		try {
-			session = await opentok.createSession();
-		} catch(err) {
-			log.error(err);
-		}
 
-		classData.session = session.sessionId;
-	}
+  if (data.participants) {
+    delete data.participants;
+  }
 
-	try {
-		c = await Class.findOneAndUpdate(
-			{ id: req.params.id, instructor: req.ctx.userData },
-			{ $set: data }
-		);
-	} catch (err) {
-		log.warn("error updating class", err);
-		return res.status(400).json({
-			message: "Error updating class",
-			error: err,
-		});
-	}
+  try {
+    c = await Class.findOneAndUpdate(
+      { id: id, instructor: req.ctx.userData },
+      { $set: data }
+    );
+  } catch (err) {
+    log.warn("error updating class", err);
+    return res.status(400).json({
+      message: "Error updating class",
+      error: err,
+    });
+  }
 
-	res.status(200).json({
-		message: "Class updated"
-	});
+  console.log("Updated class ", c);
+
+  res.status(200).json({
+    message: "Class updated"
+  });
 };
 
 /**
@@ -191,169 +190,169 @@ async function updateClass(req, res) {
  */
 async function deleteClass(req, res) {
 
-	let filter = { id: req.params.id, instructor: req.ctx.userData };
-	let c = null;
+  let filter = { id: req.params.id, instructor: req.ctx.userData };
+  let c = null;
 
-	// Check for class
-	try {
-		c = await Class.findOne(filter);
-	} catch (err) {
-		log.error("Database error", err);
-		return res.status(500).json({
-			message: "Database error",
-			error: err
-		});
-	}
+  // Check for class
+  try {
+    c = await Class.findOne(filter);
+  } catch (err) {
+    log.error("Database error", err);
+    return res.status(500).json({
+      message: "Database error",
+      error: err
+    });
+  }
 
-	// Make sure class isn't null
-	if (!c) {
-		return res.status(404).json({
-			message: "Class not found",
-		});
-	}
+  // Make sure class isn't null
+  if (!c) {
+    return res.status(404).json({
+      message: "Class not found",
+    });
+  }
 
-	// Check if class has signed up participants
-	// TODO: work out a scheme for refunds.
-	if (c.participants.length > 0) {
-		return res.status(400).json({
-			message: "Cannot remove class, it has participants.",
-		});
-	}
+  // Check if class has signed up participants
+  // TODO: work out a scheme for refunds.
+  if (c.participants.length > 0) {
+    return res.status(400).json({
+      message: "Cannot remove class, it has participants.",
+    });
+  }
 
-	// Try to delete the db document
-	try {
-		let r = await Class.deleteOne(filter);
-	} catch(err) {
-		log.warn("error removing class", err);
-		return res.status(500).json({
-			message: "Error removing class",
-			error: err,
-		});
-	}
+  // Try to delete the db document
+  try {
+    let r = await Class.deleteOne(filter);
+  } catch (err) {
+    log.warn("error removing class", err);
+    return res.status(500).json({
+      message: "Error removing class",
+      error: err,
+    });
+  }
 
-	res.status(200).json({
-		message: "Class removed",
-	});
+  res.status(200).json({
+    message: "Class removed",
+  });
 };
 
 async function addParticipant(req, res) {
 
-	let c = null;
+  let c = null;
 
-	try {
-		c = await Class.findOne({ id: req.params.id });
-	} catch(err) {
-		log.warn("database error", err);
-		return res.status(500).json({
-			message: "Database error",
-			error: err,
-		});
-	}
+  try {
+    c = await Class.findOne({ id: req.params.id });
+  } catch (err) {
+    log.warn("database error", err);
+    return res.status(500).json({
+      message: "Database error",
+      error: err,
+    });
+  }
 
-	if (!c) {
-		return res.status(404).json({ message: "Class does not exist" });
-	}
+  if (!c) {
+    return res.status(404).json({ message: "Class does not exist" });
+  }
 
-	let data = {
-		id: req.ctx.userData.id, 
-		username: req.ctx.userData.username
-	};
+  let data = {
+    id: req.ctx.userData.id,
+    username: req.ctx.userData.username
+  };
 
-	if (req.params.user_id) {
-		data.id = req.params.user_id
-	}
+  if (req.params.user_id) {
+    data.id = req.params.user_id
+  }
 
-	let exists = false;
-	c.participants.forEach(function(p) {
-		if (p.id == data.id) {
-			exists = true;
-		}
-	});
+  let exists = false;
+  c.participants.forEach(function (p) {
+    if (p.id == data.id) {
+      exists = true;
+    }
+  });
 
-	if (exists) {
-		return res.status(400).json({ message: "User already added to class" });
-	}
+  if (exists) {
+    return res.status(400).json({ message: "User already added to class" });
+  }
 
-	c.participants.push(data);
-	c.available_spots = c.available_spots - 1;
+  c.participants.push(data);
+  c.available_spots = c.available_spots - 1;
 
-	try {
-		await Class.updateOne({ id: req.params.id }, c);
-	} catch (err) {
-		log.warn("error updating class", err);
-		return res.status(400).json({
-			message: "Error adding participant",
-			error: err,
-		});
-	}
+  try {
+    await Class.updateOne({ id: req.params.id }, c);
+  } catch (err) {
+    log.warn("error updating class", err);
+    return res.status(400).json({
+      message: "Error adding participant",
+      error: err,
+    });
+  }
 
-	res.status(200).json({
-		message: "User added to class"
-	});
+  res.status(200).json({
+    message: "User added to class"
+  });
 }
 
 async function removeParticipant(req, res) {
-	let c = null;
+  let c = null;
 
-	try {
-		c = await Class.findOne({ id: req.params.id });
-	} catch(err) {
-		log.warn("database error", err);
-		return res.status(500).json({
-			message: "Database error",
-			error: err,
-		});
-	}
+  try {
+    c = await Class.findOne({ id: req.params.id });
+  } catch (err) {
+    log.warn("database error", err);
+    return res.status(500).json({
+      message: "Database error",
+      error: err,
+    });
+  }
 
-	if (!c) {
-		return res.status(404).json({ message: "Class does not exist" });
-	}
+  if (!c) {
+    return res.status(404).json({ message: "Class does not exist" });
+  }
 
-	let data = {
-		id: req.ctx.userData.id, 
-		username: req.ctx.userData.username
-	};
+  let data = {
+    id: req.ctx.userData.id,
+    username: req.ctx.userData.username
+  };
 
-	if (req.params.user_id) {
-		data.id = req.params.user_id
-	}
+  if (req.params.user_id) {
+    data.id = req.params.user_id
+  }
 
-	let index = -1;
-	for(var i = 0; i < c.participants.length; i++) {
-		if (c.participants[i].id == data.id) {
-			index = i;
-		}
-	}
+  let index = -1;
+  for (var i = 0; i < c.participants.length; i++) {
+    if (c.participants[i].id == data.id) {
+      index = i;
+    }
+  }
 
-	if (index == -1) {
-		return res.status(400).json({ message: "User not in class" });
-	}
-	
-	c.participants.splice(index, 1);
-	c.available_spots = c.available_spots + 1;
+  if (index == -1) {
+    return res.status(400).json({ message: "User not in class" });
+  }
 
-	try {
-		await Class.updateOne({ id: req.params.id }, c);
-	} catch (err) {
-		log.warn("error updating class", err);
-		return res.status(400).json({
-			message: "Error adding participant",
-			error: err,
-		});
-	}
+  c.participants.splice(index, 1);
+  c.available_spots = c.available_spots + 1;
 
-	res.status(200).json({
-		message: "User removed from class"
-	});
-	
+  try {
+    await Class.updateOne({ id: req.params.id }, c);
+  } catch (err) {
+    log.warn("error updating class", err);
+    return res.status(400).json({
+      message: "Error adding participant",
+      error: err,
+    });
+  }
+
+  res.status(200).json({
+    message: "User removed from class"
+  });
+
 }
 
 module.exports = {
-	deleteClass,
-	updateClass,
-	getClass,
-	getClasses,
-	createClass,
-	addParticipant,
-	removeParticipant,
+  deleteClass,
+  updateClass,
+  getClass,
+  getClasses,
+  createClass,
+  addParticipant,
+  removeParticipant,
 };
