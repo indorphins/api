b1c5240d53e4b96a11052a6a3673423eac8da9d2
afloesch <andrew@vapi.com>
2020-10-11 20:38:53 -0700

    add file

---
+++ reporting/feedback.js
@@ -0,0 +1,65 @@
+const ClassFeedback = require('../src/db/ClassFeedback');
+
+async function classFeedbackForms() {
+
+  let format = {
+    $project: {
+      year: { $year: "$created_date"},
+      week: { $week: "$created_date" },
+      instructorId: "$instructorId",
+      instructorRating: "$instructorRating",
+      classRating: "$classRating",
+      videoRating: "$videoRating",
+    } 
+  }
+
+  let group = {
+    $group: {
+      _id: {
+        year: "$year",
+        week: "$week",
+        instructorId: "$instructorId",
+      },
+      instructorRating: {
+        $avg: "$instructorRating",
+      },
+      classRating: {
+        $avg: "$classRating",
+      },
+      videoRating: {
+        $avg: "$videoRating",
+      }
+    }
+  };
+
+  let report = {
+    $project: {
+      week: "$_id.week",
+      year: "$id_.year",
+      instructorId: "$_id.instructorId",
+      averageInstructorRating: "$instructorRating",
+      averageClassRating: "$classRating",
+      averageVideoRating: "$videoRating",
+    }
+  }
+
+  let save = {
+    $merge: {
+      into: "instructorreportings",
+      on: "_id",
+      whenMatched: "merge",
+      whenNotMatched: "insert",
+    }
+  }
+
+  return ClassFeedback.aggregate([
+    format,
+    group,
+    report,
+    save,
+  ])
+}
+
+module.exports = {
+  classFeedbackForms: classFeedbackForms
+};
