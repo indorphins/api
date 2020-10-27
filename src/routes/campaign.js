a8e8b3a63f367ba6cedf6b7d8015dead3fad2b15
afloesch <andrew@vapi.com>
2020-10-26 21:05:15 -0700

    remove auth requirements for campaign endpoint

--- src/routes/campaign.js
+++ src/routes/campaign.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const middleware = require('../middleware');
+//const middleware = require('../middleware');
 const campaign = require('../handlers/campaign');
 
-router.get('/id/:id', middleware.authentication);
+//router.get('/id/:id', middleware.authentication);
 router.get('/id/:id', campaign.get);
 
 module.exports = router;
