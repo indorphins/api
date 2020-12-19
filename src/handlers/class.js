3560c29bd1cf8910b11cad43c4016fcbe78231b4
Alex Lindsay <alindsaydev@gmail.com>
2020-12-18 16:09:02 -0800

    refund and payout tweaks

--- src/handlers/class.js
+++ src/handlers/class.js
@@ -247,8 +247,6 @@ async function deleteClass(req, res) {
         log.warn("Couldn't find corresponding subscriptions for user", err);
       }
 
-      console.log("Found user's subs ", subscriptions);
-      
       if (subscriptions && subscriptions[0]) {
         subscription = subscriptions[0];
       }
