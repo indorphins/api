d9f33006452b2fd8594476de587bf94810d423dc
Alex Lindsay <alindsaydev@gmail.com>
2020-12-21 18:05:15 -0800

    get unique participants emails api

--- src/handlers/instructor.js
+++ src/handlers/instructor.js
@@ -1,4 +1,7 @@
 const User = require('../db/User');
+const Class = require('../db/Class');
+const log = require('../log');
+const { asyncForEach } = require('../utils/index');
 
 async function getList(req, res) {
 	let page = req.query.page ? Number(req.query.page) - 1 : 0;
@@ -60,7 +63,73 @@ async function get(req, res) {
 	});
 }
 
+// Fetches all the usernames and emails of every unique user that has taken a class with an instructor
+async function uniqueParticipants(req, res) {
+  const userData = req.ctx.userData;
+
+  if (userData.type !== 'admin') {
+    log.warn("Only admins can fetch this data");
+    return res.status(404).json({
+      message: "Only admins can fetch this data"
+    })
+  }
+
+  let instructors;
+  try {
+    instructors = await User.find({ type: 'instructor' });
+  } catch (err) {
+    log.warn("uniqueParticipants report database error ", err);
+    return res.status(500).json({
+      message: 'database error'
+    })
+  }
+
+  let instructorMap = {};
+  await asyncForEach(instructors, async i => {
+    let classes;
+    try {
+      classes = await Class.find({ instructor: i.id });
+    } catch (err) {
+      log.warn("uniqueParticipants Database error ", err);
+      return res.status(500).json({
+        message: 'database error'
+      })
+    }
+
+    let participants = [];
+    if (classes && classes.length > 0) {
+      classes.map(c => {
+        let userIds = c.participants.map(participant => {
+          return participant.id;
+        });
+        participants = [...participants, ...userIds];
+      })
+    }
+
+    let users;
+    try {
+      users = await User.find({ id: { $in: participants }});
+    } catch (err) {
+      log.warn("database error", err);
+      return res.status(500).json({
+        message: 'database error'
+      })    }
+
+    let data = users.map(user => {
+      return {
+        username: user.username,
+        email: user.email,
+      }
+    });
+
+    instructorMap[i.username] = data;
+  })
+
+  return res.status(200).json(instructorMap);
+}
+
 module.exports = {
   get,
-	getList
+  getList,
+  uniqueParticipants
 };
--- src/routes/instructor.js
+++ src/routes/instructor.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const instructor = require('../handlers/instructor');
+const middleware = require('../middleware');
 
 let router = express.Router();
 router.get('/', instructor.getList);
 router.get('/:id', instructor.get);
+
+router.get('/participants/unique', middleware.authentication);
+router.get('/participants/unique', instructor.uniqueParticipants);
 
 module.exports = router;
